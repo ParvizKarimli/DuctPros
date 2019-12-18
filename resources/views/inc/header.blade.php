0e62b1b607f5903a1697af85f7983d3385ed4491
Parviz Karimli <kerimlip@gmail.com>
2019-12-18 10:47:46 +0400

    Added check if the user is logged in, if true then show dropdown with dashboard and logout links in header navbar

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -34,6 +34,25 @@
                     <li class="nav-item">
                         <a class="nav-link" href="/#contact-us">Contact Us</a>
                     </li>
+                    @if(auth()->check())
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" data-animations="fadeInDown">
+                                <a class="dropdown-item" href="{{ route('dashboard') }}">Dashboard</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </div>
+                        </li>
+                    @endif
 
                 </ul>
             </div>
